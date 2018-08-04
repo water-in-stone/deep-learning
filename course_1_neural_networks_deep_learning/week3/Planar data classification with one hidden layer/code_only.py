90ce97cbc69da4db9e1943e66b3f413d706a4f0c
翼阳 <heyang.why@alibaba-inc.com>
2018-08-04 10:26:26 +0800

    fix bug

--- course_1_neural_networks_deep_learning/week3/Planar data classification with one hidden layer/code_only.py
+++ course_1_neural_networks_deep_learning/week3/Planar data classification with one hidden layer/code_only.py
@@ -396,6 +396,7 @@ print("predictions mean = " + str(np.mean(predictions)))
 parameters = nn_model(X, Y, n_h=4, num_iterations=10000, print_cost=True)
 
 # Plot the decision boundary
+# 画图前需要转换Y矩阵
 plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y.reshape(400,))
 plt.title("Decision Boundary for hidden layer size " + str(4))
 
